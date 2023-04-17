142aff698ade17900c621b80c7be94a734e81c48
Tiesen <ttien56906@gmail.com>
2023-04-18 02:27:35 +0700

    add css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
+// Modules
 const express = require('express')
-const morgan = require('morgan')
+const path = require('path')
+const { engine } = require ('express-handlebars')
 
+// Create App
 const app = express()
 const port = 3000
 
+// Add Public to App
+app.use(express.static(path.join(__dirname, './public')))
+
+// HTTP Logger
+const morgan = require('morgan')
 app.use(morgan('combined'))
-app.get('/home', (req, res) => res.send(`
-  <h1>Hello<h1>
-`))
-app.listen(port, () => console.log(`Listening on port: ${port}`));
+
+
+// Template Engine
+app.engine('html', engine({
+  extname: '.html'
+}))
+app.set('view engine', 'html')
+app.set("views", path.join(__dirname, 'resource/views'))
+
+// Route
+app.get('/home', (req, res) => {
+    res.render('home');
+});
+
+app.get('/news', (req, res) => {
+    res.render('news');
+});
+
+
+app.listen(port, () => console.log(`Listening on port: ${port}`))
+
+
