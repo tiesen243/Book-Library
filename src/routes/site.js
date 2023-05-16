7192030c2b0c85766c939c31e173873aacebc64b
Tiesen <ttien56906@gmail.com>
2023-05-17 02:36:07 +0700

    Update css

--- src/routes/site.js
+++ src/routes/site.js
@@ -4,6 +4,6 @@ const router = express.Router()
 const siteController = require('../app/controllers/SiteController')
 
 router.use('/search', siteController.search)
-router.use('/', siteController.home)
+router.use('/home', siteController.home)
 
 module.exports = router
